015dfbe7abfb67354c8b6da1f11d807f62382f71
Julian Unkel <julian.unkel@gmail.com>
2019-04-19 16:10:33 +0200

    Refactor describe functions

--- R/describe.R
+++ R/describe.R
@@ -1,7 +1,36 @@
+#' Provide summary functions
+#'
+#' Returns summary functions for describe functions
+#'
+#' @import dplyr
+#'
+#' @param var Variable that holds values to summarize
+#' @param na.rm a logical value indicating whether NA values should be stripped
+#'     before the computation proceeds, passed on from the describe functions.
+#'
+#' @return A list of summary functions to be used in summarise().
+summary <- function(var, na.rm) {
+  s <- list(
+    N = quo(n()),
+    Missing = quo(sum(is.na(!! var))),
+    M = quo(mean(!! var, na.rm = !! na.rm)),
+    SD = quo(sd(!! var, na.rm = !! na.rm)),
+    Min = quo(min(!! var, na.rm = !! na.rm)),
+    Max = quo(max(!! var, na.rm = !! na.rm)),
+    Range = quo(Max - Min),
+    Mdn = quo(median(!! var, na.rm = !! na.rm)),
+    Q2.5 = quo(quantile(!! var, .025, na.rm = !! na.rm)),
+    Q97.5 = quo(quantile(!! var, .975, na.rm = !! na.rm))
+  )
+  return(s)
+}
+
+
 #' Describe variables
 #'
-#' Descriptively describe one or more continous variable by several measures of
-#' central tendency and variability.
+#' Descriptively describe variables by several measures of
+#' central tendency and variability. If no variables are specified,
+#' all numeric (integer or double) variables are described.
 #'
 #' @import dplyr
 #' @import tidyr
@@ -15,24 +44,24 @@
 #'     variables
 #'
 #' @examples describe(mtcars, disp, hp, mpg)
+#'   describe(iris)
 #'
 #' @export
 describe <- function(data, ..., na.rm = T) {
-
   vars <- quos(...)
+
+  if (length(vars) == 0) {
+    vars <- data %>%
+      select_if(~ is.double(.) || is.integer(.)) %>%
+      names %>%
+      syms
+  }
 
   data %>%
     select(!!! vars) %>%
     gather(variable, val, !!! vars) %>%
     group_by(variable) %>%
-    summarise(N = n(),
-              Missing = sum(is.na(val)),
-              M = mean(val, na.rm = na.rm),
-              SD = sd(val, na.rm = na.rm),
-              Min = min(val, na.rm = na.rm),
-              Max = max(val, na.rm = na.rm),
-              Range = Max - Min,
-              Mdn = median(val, na.rm = na.rm))
+    summarise(!!! summary(quo(val), na.rm))
 
 }
 
@@ -62,44 +91,6 @@ describe_groups <- function(data, var, ..., na.rm = T) {
   data %>%
     select(!! var, !!! groupVars) %>%
     group_by(!!! groupVars) %>%
-    summarise(N = n(),
-              Missing = sum(is.na(!! var)),
-              M = mean(!! var, na.rm = na.rm),
-              SD = sd(!! var, na.rm = na.rm),
-              Min = min(!! var, na.rm = na.rm),
-              Max = max(!! var, na.rm = na.rm),
-              Range = Max - Min,
-              Mdn = median(!! var, na.rm = na.rm))
-
-}
-
-#' Describe categorial variable
-#'
-#' Compute frequency table for a categorial variable (includes percentages
-#'  and cumulative percentages).
-#'
-#' @import dplyr
-#'
-#' @param data A (tidy) dataset
-#' @param var Variable to describe
-#' @param sort A logical value indicating whether categories should be sorted
-#'     descending by highest value. Defaults to FALSE.
-#' @return A tibble containing absolute frequencies, percentages and cumulative
-#'     percentages.
-#' @examples describe_cat(mtcars, cyl)
-#'
-#' @export
-describe_cat <- function(data, var, sort = F) {
-
-  var <- enquo(var)
-
-  data %>%
-    group_by(!! var) %>%
-    count(!! var, sort = sort) %>%
-    ungroup() %>%
-    mutate(
-      perc = n / sum(n),
-      cperc = cumsum(perc)
-    )
+    summarise(!!! summary(var, na.rm))
 
 }
